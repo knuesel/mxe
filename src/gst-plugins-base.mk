237b53b85edaaa5114fadba0cee99bc627e86046
Jeremie Knuesel <jeremie.knusel@sensefly.com>
2014-12-11 10:02:56 +0100

    gst-plugins-base: disable dependency on ogg, vorbis, theora

--- src/gst-plugins-base.mk
+++ src/gst-plugins-base.mk
@@ -8,7 +8,8 @@ $(PKG)_CHECKSUM := f46c9f096a641623b6d69f6c1aaba0c87a1dd71e
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := http://gstreamer.freedesktop.org/src/$(PKG)/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc glib libxml2 gstreamer liboil pango ogg vorbis theora
+# $(PKG)_DEPS     := gcc glib libxml2 gstreamer liboil pango ogg vorbis theora
+$(PKG)_DEPS     := gcc glib libxml2 gstreamer liboil pango
 
 $(PKG)_UPDATE = $(subst gstreamer/refs,gst-plugins-base/refs,$(gstreamer_UPDATE))
 
